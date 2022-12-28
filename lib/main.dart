3d1318ffab465d908242b05d2ab55368c811fb9c
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-12-28 05:54:58 +0530

    first to skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,113 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+List<BoxShadow> shadowList = [
+  BoxShadow(
+      color: Colors.green[300]!, blurRadius: 30, offset: const Offset(0, 10))
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      title: "Go router",
-      theme: _appTheme,
-    );
-  }
-}
-
-final _appTheme = ThemeData(
-  primarySwatch: Colors.amber,
-  textTheme: const TextTheme(
-    displayLarge: TextStyle(
-      fontWeight: FontWeight.w900,
-      fontSize: 34,
-      color: Colors.black,
-    ),
-  ),
-);
-
-final GoRouter _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: "/",
-      builder: (context, state) => const FirstPage(),
-    ),
-    GoRoute(
-      path: "/secondpage",
-      builder: (context, state) => const SecondPage(),
-    )
-  ],
-);
-
-class FirstPage extends StatefulWidget {
-  const FirstPage({super.key});
-
-  @override
-  State<FirstPage> createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'First Page',
-        ),
+    return MaterialApp(
+      title: "Skeleton Text Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'This is First Page:',
-              style: TextStyle(
-                fontSize: 50.00,
-                fontWeight: FontWeight.bold,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          physics: const BouncingScrollPhysics(),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 240,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SkeletonAnimation(
+                      shimmerColor: Colors.green,
+                      borderRadius: BorderRadius.circular(20),
+                      shimmerDuration: 1000,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green[300],
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: shadowList,
+                        ),
+                        margin: const EdgeInsets.only(top: 40),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 60, bottom: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        boxShadow: shadowList,
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 15.0, bottom: 5.0),
+                            child: SkeletonAnimation(
+                              borderRadius: BorderRadius.circular(10.0),
+                              shimmerColor: index % 2 != 0
+                                  ? Colors.green
+                                  : Colors.white54,
+                              child: Container(
+                                height: 30,
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Colors.green[300]),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15.0),
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 5.0),
+                              child: SkeletonAnimation(
+                                borderRadius: BorderRadius.circular(10.0),
+                                shimmerColor: index % 2 != 0
+                                    ? Colors.green
+                                    : Colors.white54,
+                                child: Container(
+                                  width: 60,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: Colors.green[300]),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            ElevatedButton(
-              onPressed: () => context.go("/secondpage"),
-              child: const Text("Go to Second page"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Second Page',
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'This is Second Page:',
-              style: TextStyle(
-                fontSize: 50.00,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(20.00),
-              width: 150.00,
-              child: ElevatedButton(
-                onPressed: () => context.go("/"),
-                child: const Text("Go to First page"),
-              ),
-            )
-          ],
+            );
+          },
         ),
       ),
     );
